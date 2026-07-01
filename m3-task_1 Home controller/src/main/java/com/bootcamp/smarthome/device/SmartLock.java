93c4ff12ace54564a65a9a00714058148ed9cab2
Edgaras <edgarasdaugevicius@gmail.com>
2026-07-01 12:15:19 +0300

    Added exception handling (task 2) (#2)

--- m3-task_1 Home controller/src/main/java/com/bootcamp/smarthome/device/SmartLock.java
+++ m3-task_1 Home controller/src/main/java/com/bootcamp/smarthome/device/SmartLock.java
@@ -1,5 +1,8 @@
 package com.bootcamp.smarthome.device;
 
+import com.bootcamp.smarthome.exception.HomeAutomationException;
+import com.bootcamp.smarthome.exception.InvalidCommandException;
+
 /**
  * A PIN-protected smart door lock.
  *
@@ -24,12 +27,15 @@ public class SmartLock extends Device {
     /**
      * Validates the supplied PIN against the stored PIN.
      */
-    public void validatePin(String pin) {
+    public void validatePin(String pin) throws InvalidCommandException {
+        if (pin == null) {
+            throw new InvalidCommandException("Pin is required");
+        }
         if (pin.equals(storedPin)) {
             isLocked = false;
             System.out.println(getName() + " unlocked successfully.");
         } else {
-            System.out.println("SECURITY ALERT: Incorrect PIN entered for " + getName() + ".");
+            throw new InvalidCommandException(String.format("%s is incorrect", pin));
         }
     }
 
@@ -39,7 +45,7 @@ public class SmartLock extends Device {
     }
 
     @Override
-    public void executeCommand(String command) {
+    public void executeCommand(String command) throws HomeAutomationException {
         if (command.startsWith("UNLOCK")) {
             String[] parts = command.split(" ");
             String pin = (parts.length > 1) ? parts[1] : null;
@@ -51,7 +57,7 @@ public class SmartLock extends Device {
         } else if (command.equals("TURN_OFF")) {
             turnOff();
         } else {
-            System.out.println("Unknown command for SmartLock '" + getName() + "': " + command);
+            throw new InvalidCommandException("Unknown command for SmartLock '" + getName() + "': " + command);
         }
     }
 
