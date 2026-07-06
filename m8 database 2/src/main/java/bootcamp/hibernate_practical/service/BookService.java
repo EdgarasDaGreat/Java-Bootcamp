503cc6f626edb4745669343aaaa546ac30fb5fda
Edgaras <edgarasdaugevicius@gmail.com>
2026-07-06 12:23:24 +0300

    M9 spring security (#8)
    * Added m9 task securityFilterChain + fixed some issues pointed out by feedback on m8

--- m8 database 2/src/main/java/bootcamp/hibernate_practical/service/BookService.java
+++ m8 database 2/src/main/java/bootcamp/hibernate_practical/service/BookService.java
@@ -6,7 +6,10 @@ import bootcamp.hibernate_practical.dto.UpdateBookRequestDto;
 import bootcamp.hibernate_practical.entity.Book;
 import bootcamp.hibernate_practical.mapper.BookMapper;
 import bootcamp.hibernate_practical.repository.BookRepository;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,20 +18,20 @@ import java.util.Optional;
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final BookMapper mapper;
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+        this.mapper = new BookMapper();
     }
 
     public BookResponseDto createBook(CreateBookRequestDto request) {
-        BookMapper mapper = new BookMapper();
         Book book = mapper.toCreateEntity(request);
         bookRepository.save(book);
         return mapper.toDto(book);
     }
 
     public List<BookResponseDto> getAllBooks() {
-        BookMapper mapper = new BookMapper();
         List<Book> bookEntities = bookRepository.findAll();
         List<BookResponseDto> bookResponseDtos = new ArrayList<>();
         for (Book bookEntity : bookEntities) {
@@ -38,26 +41,26 @@ public class BookService {
     }
 
     public BookResponseDto getBookById(Long id) {
-        BookMapper mapper = new BookMapper();
-        Book book = bookRepository.findById(id).orElseThrow(()-> new RuntimeException("Book not found with id:" + id));
+        Book book = bookRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with id: " + id + " not found"));
         return mapper.toDto(book);
     }
 
     public BookResponseDto updateBook(Long id, UpdateBookRequestDto request) {
-        BookMapper mapper = new BookMapper();
-        Book book = bookRepository.findById(id).orElseThrow(()-> new RuntimeException("Book not found with id:" + id));
+        Book book = bookRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with id: " + id + " not found"));
         Book updatedBook = mapper.toUpdateEntity(book, request);
         bookRepository.save(updatedBook);
         return mapper.toDto(updatedBook);
     }
 
     public void deleteBook(Long id) {
-        Book book = bookRepository.findById(id).orElseThrow(()-> new RuntimeException("Book not found with id:" + id));
+        Book book = bookRepository
+                .findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book with id: " + id + " not found"));
         bookRepository.delete(book);
     }
 
     public List<BookResponseDto> findByAuthor(String author) {
-        BookMapper mapper = new BookMapper();
         List<Book> booksByAuthor = new ArrayList<>();
         booksByAuthor = bookRepository.findByAuthor(author);
         List<BookResponseDto> bookResponseDtos = new ArrayList<>();
@@ -68,7 +71,6 @@ public class BookService {
     }
 
     public List<BookResponseDto> findAvailableBooks() {
-        BookMapper mapper = new BookMapper();
         List<Book> availableBooks = bookRepository.findByAvailableTrue();
         List<BookResponseDto> bookResponseDtos = new ArrayList<>();
         for (Book book : availableBooks) {
